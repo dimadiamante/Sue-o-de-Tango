4c887d1efb7f33d5bca12f3f4e80492fc6aede01
Дмитрий Ячмень <37130745+dimadiamante@users.noreply.github.com>
2025-10-26 12:08:15 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Sueno-de-Tango/', // точное имя репозитория
-})
+base: '/Sueno-de-Tango/',
