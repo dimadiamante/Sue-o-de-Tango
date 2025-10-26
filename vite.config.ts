356285d609a5c9144c2c5ac18f025479690ab757
Дмитрий Ячмень <37130745+dimadiamante@users.noreply.github.com>
2025-10-26 12:32:30 +0200

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// ВАЖНО: имя репозитория чувствительно к регистру в base
+export default defineConfig({
+  plugins: [react()],
+  base: '/Sueno-de-Tango/',
+})
